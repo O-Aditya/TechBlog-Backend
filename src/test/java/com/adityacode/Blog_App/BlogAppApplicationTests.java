95a81e0749269d963a4d58f2914fdf20299f87cb
Aditya Maurya <mauryaaditya7405@gmail.com>
2025-06-23 14:43:10 +0530

    Project Setup

---
+++ src/test/java/com/adityacode/Blog_App/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityacode.Blog_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
