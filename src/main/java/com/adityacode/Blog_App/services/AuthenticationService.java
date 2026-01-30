741301ae72a348a78c097ff40e4a9c91eae8cd3a
Aditya Maurya <mauryaaditya7405@gmail.com>
2026-01-31 00:52:57 +0530

    Add AuthenticationService

---
+++ src/main/java/com/adityacode/Blog_App/services/AuthenticationService.java
@@ -0,0 +1,14 @@
+package com.adityacode.Blog_App.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.io.Serial;
+
+
+@Service
+public interface AuthenticationService {
+
+    UserDetails authenticate(String username, String password);
+    String generateToken(UserDetails userDetails);
+}
